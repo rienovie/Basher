37ecedab10bce1d5c4c37d897c535313e8479d3d
Rienovie <fisher.vince@gmail.com>
2024-06-22 15:29:45 -0400

    Start of plugin

--- lua/Basher.lua
+++ lua/Basher.lua
@@ -4,7 +4,7 @@ local module = require("Basher.module")
 ---@class Config
 ---@field opt string Your config option
 local config = {
-  opt = "Hello!",
+	opt = "Hello!",
 }
 
 ---@class MyModule
@@ -17,11 +17,31 @@ M.config = config
 -- you can define your setup function here. Usually configurations can be merged, accepting outside params and
 -- you can also put some validation here for those.
 M.setup = function(args)
-  M.config = vim.tbl_deep_extend("force", M.config, args or {})
+	M.config = vim.tbl_deep_extend("force", M.config, args or {})
 end
 
-M.hello = function()
-  return module.my_first_function(M.config.opt)
+M.showMainWin = function()
+	return module.show_main_win()
+end
+
+M.handleEnterKey = function()
+	return module.handle_enter_key()
+end
+
+M.closeMainWin = function()
+	return module.close_main_win()
+end
+
+M.moveDown = function()
+	return module.move_down()
+end
+
+M.moveUp = function()
+	return module.move_up()
+end
+
+M.runScript = function(which)
+	return module.run_script(which)
 end
 
 return M
--- lua/Basher/module.lua
+++ lua/Basher/module.lua
@@ -1,9 +1,97 @@
 ---@class CustomModule
+
+local popup = require("plenary.popup")
+
 local M = {}
 
----@return string
-M.my_first_function = function(greeting)
-  return greeting
+local function define_popup_mappings(buf)
+	vim.api.nvim_buf_set_keymap(
+		buf,
+		"n",
+		"<CR>",
+		":lua require('Basher').handleEnterKey()<CR>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_buf_set_keymap(
+		buf,
+		"n",
+		"<Esc>",
+		":lua require('Basher').closeMainWin()<CR>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_buf_set_keymap(
+		buf,
+		"n",
+		"<S-d>",
+		":lua require('Basher').moveDown()<CR>",
+		{ noremap = true, silent = true, desc = "[M]ove [D]own" }
+	)
+	vim.api.nvim_buf_set_keymap(
+		buf,
+		"n",
+		"<S-u>",
+		":lua require('Basher').moveUp()<CR>",
+		{ noremap = true, silent = true, desc = "[M]ove [U]p" }
+	)
+	vim.api.nvim_buf_set_keymap(
+		buf,
+		"n",
+		"1",
+		":lua require('Basher').runScript(1)<CR>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_buf_set_keymap(
+		buf,
+		"n",
+		"2",
+		":lua require('Basher').runScript(2)<CR>",
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_buf_set_keymap(
+		buf,
+		"n",
+		"3",
+		":lua require('Basher').runScript(3)<CR>",
+		{ noremap = true, silent = true }
+	)
+end
+
+M.handle_enter_key = function()
+	M.run_script(vim.fn.line("."))
+end
+
+M.close_main_win = function()
+	vim.api.nvim_win_close(0, true)
+end
+
+M.move_down = function()
+	print("Down pressed")
+end
+
+M.move_up = function()
+	print("Up pressed")
+end
+
+M.run_script = function(which)
+	print("Called run on script : " .. which)
+	M.close_main_win()
+end
+
+M.show_main_win = function()
+	local buf = vim.api.nvim_create_buf(false, true)
+	local popupOpts = {
+		title = "Basher",
+		line = math.floor(((vim.o.lines - 5) / 2) - 1),
+		col = math.floor((vim.o.columns - 60) / 2),
+		minwidth = 60,
+		minheight = 5,
+		borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+	}
+	local _, pu = popup.create(buf, popupOpts)
+	vim.api.nvim_win_set_var(pu.border.win_id, "winhl", "Basher")
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Option 1", "Option 2", "Option 3" })
+	vim.opt_local.number = true
+	define_popup_mappings(buf)
 end
 
 return M
